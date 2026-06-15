d7b76bcc4cabf9d09b96798d0c5675b7c04911ba
lovell06 <xuansontrannguyen06@gmail.com>
2026-06-16 04:22:48 +0700

    fix UserRole.Customer(id) => 2

--- src/FastFoodOrderingSystem.Domain/Common/Enums/UserRole.cs
+++ src/FastFoodOrderingSystem.Domain/Common/Enums/UserRole.cs
@@ -6,7 +6,8 @@ public sealed class UserRole : SmartEnum<UserRole>
 {
     public static readonly UserRole Admin = new UserRole(0, "admin");
     public static readonly UserRole Employee = new UserRole(1, "employee");
-    public static readonly UserRole Customer = new UserRole(1, "customer");
+    public static readonly UserRole Customer = new UserRole(2, "customer");
+
     private UserRole(int id, string roleCode) : base(id, roleCode)
     {
     }
