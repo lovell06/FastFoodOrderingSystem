3bdc32c4499fdcb51afc73744f209911658f213f
lovell06 <xuansontrannguyen06@gmail.com>
2026-07-02 17:18:31 +0700

    Application: add ILogger<RegisterHandler>

--- src/FastFoodOrderingSystem.Application/Features/Auth/Register/RegisterHandler.cs
+++ src/FastFoodOrderingSystem.Application/Features/Auth/Register/RegisterHandler.cs
@@ -4,10 +4,10 @@ using FastFoodOrderingSystem.Application.Abstractions.Configurations;
 using FastFoodOrderingSystem.Application.Abstractions.Persistence;
 using FastFoodOrderingSystem.Application.Common.Errors;
 using FastFoodOrderingSystem.Application.Common.Results;
-using FastFoodOrderingSystem.Domain.Common.Exceptions;
 using FastFoodOrderingSystem.Domain.Common.ValueObjects;
 using FastFoodOrderingSystem.Domain.Common.ValueObjects.Exceptions;
 using FastFoodOrderingSystem.Domain.Users;
+using Microsoft.Extensions.Logging;
 
 namespace FastFoodOrderingSystem.Application.Features.Auth.Register;
 
@@ -15,36 +15,41 @@ public sealed class RegisterHandler
 {
     private readonly IUserRepository _userRepository;
     private readonly IPendingRegistrationStore _pendingRegistrationStore;
-    private readonly IUnitWork _unitWork;
     private readonly IPasswordHashService _passwordHashService;
     private readonly IOtpService _otpService;
     private readonly IOtpHashService _otpHashService;
     private readonly IOtpConfiguration _otpConfiguration;
+    private readonly ILogger<RegisterHandler> _logger;
     public RegisterHandler(
         IUserRepository userRepository, 
         IPendingRegistrationStore pendingRegistrationStore,
-        IUnitWork unitWork,
         IPasswordHashService passwordHashService,
         IOtpService otpService,
         IOtpHashService otpHashService,
-        IOtpConfiguration otpConfiguration)
+        IOtpConfiguration otpConfiguration,
+        ILogger<RegisterHandler> logger)
     {
         _userRepository = userRepository;
         _pendingRegistrationStore = pendingRegistrationStore;
-        _unitWork = unitWork;
         _passwordHashService = passwordHashService;
         _otpService = otpService;
         _otpHashService = otpHashService;
         _otpConfiguration = otpConfiguration;
+        _logger = logger;
     }
     public async Task<Result<RegisterResponse>> HandleAsync(RegisterCommand command)
     {
+        var now = DateTime.Now;
         try
         {
             var email = Email.Create(command.Email);
 
             if (await _userRepository.EmailAlreadyExistedAsync(email))
-                return Result<RegisterResponse>.Failure(RegisterError.EmailAlreadyExisted(email));
+            {
+                var err = RegisterError.EmailAlreadyExisted(email);
+                _logger.LogError($"{now} RegisterHandler: Code({err.Code}); Message({err.Message}).");
+                return Result<RegisterResponse>.Failure(err);
+            }
 
             var fullName = FullName.Create(command.FullName);
             var password = Password.Create(command.Password);
@@ -59,35 +64,45 @@ public sealed class RegisterHandler
                 passwordHash: passwordHash,
                 phone: phoneNumber,
                 otpCodeHash: otpCodeHash,
-                expiresAt: DateTime.Now.AddMinutes(_otpConfiguration.Expiration)
+                expiresAt: now.AddMinutes(_otpConfiguration.Expiration)
             );
 
-            await _pendingRegistrationStore.SaveAsync(pendingRegistration: pending);
+            if (!await _pendingRegistrationStore.SaveAsync(pendingRegistration: pending))
+            {
+                _logger.LogError($"{now} RegisterHandler: Pending registration store {email.Value} failed.");
+            }
 
-            return Result<RegisterResponse>.Success(new RegisterResponse(""));
+            _logger.LogInformation($"{now} RegisterHandler: Pending registration store {email.Value} successful.");
+            return Result<RegisterResponse>.Success(new RegisterResponse($"OTP code sent to email {email.Value}."));
         }
         catch (InvalidEmailException exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Code({exception.Code}); Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(RegisterError.InvalidEmail(exception));
         }
         catch (InvalidFullNameException exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Code({exception.Code}); Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(RegisterError.InvalidFullName(exception));
         }
         catch (InvalidPasswordException exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Code({exception.Code}); Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(RegisterError.InvalidPassword(exception));
         }
         catch (InvalidPhoneNumberException exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Code({exception.Code}); Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(RegisterError.InvalidPhoneNumber(exception));
         }
         catch (InvalidOtpCodeException exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Code({exception.Code}); Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(RegisterError.InvalidOtpCode(exception));
         }
         catch (Exception exception)
         {
+            _logger.LogError($"{now} RegisterHandler: Message({exception.Message}).");
             return Result<RegisterResponse>.Failure(SystemError.Unexpected);
         }
     }
