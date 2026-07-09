403a932d6a95da3ea6278071adcebedac54d0eb5
lovell06 <xuansontrannguyen06@gmail.com>
2026-07-10 01:07:16 +0700

    Api: refactoring Controllers

---
+++ src/FastFoodOrderingSystem.Api/Controllers/Auth/AuthController.cs
@@ -0,0 +1,116 @@
+using FastFoodOrderingSystem.Api.Contracts.Authentication;
+using FastFoodOrderingSystem.Application.Common.Handlers;
+using FastFoodOrderingSystem.Application.Common.Results;
+using FastFoodOrderingSystem.Application.Features.Auth.Login;
+using FastFoodOrderingSystem.Application.Features.Auth.Logout;
+using FastFoodOrderingSystem.Application.Features.Auth.Refresh;
+using FastFoodOrderingSystem.Application.Features.Auth.Register;
+using FastFoodOrderingSystem.Application.Features.Auth.VerifyOtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoodOrderingSystem.Api.Controllers.Auth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IHandler<LoginCommand, Result<LoginResponse>> _loginHandler;
+        private readonly IHandler<LogoutCommand, Result<LogoutResponse>> _logoutHandler;
+        private readonly IHandler<RefreshTokenCommand, Result<RefreshTokenResponse>> _refreshTokenHandler;
+        private readonly IHandler<RegisterCommand, Result<RegisterResponse>> _registerHandler;
+        private readonly IHandler<VerifyOtpCommand, Result<VerifyOtpResponse>> _verifyOtpHandler;
+
+        public AuthController(
+            IHandler<LoginCommand, Result<LoginResponse>> loginHandler, 
+            IHandler<LogoutCommand, Result<LogoutResponse>> logoutHandler, 
+            IHandler<RefreshTokenCommand, Result<RefreshTokenResponse>> refreshTokenHandler, 
+            IHandler<RegisterCommand, Result<RegisterResponse>> registerHandler, 
+            IHandler<VerifyOtpCommand, Result<VerifyOtpResponse>> verifyOtpHandler)
+        {
+            _loginHandler = loginHandler;
+            _logoutHandler = logoutHandler;
+            _refreshTokenHandler = refreshTokenHandler;
+            _registerHandler = registerHandler;
+            _verifyOtpHandler = verifyOtpHandler;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request, CancellationToken cancellationToken)
+        {
+            var command = request.ToCommand();
+
+            var result = await _loginHandler.HandleAsync(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                Error err = result.Error!;
+                if (err.Type == ErrorType.Validtion || err.Type == ErrorType.Failure)
+                    return BadRequest(new { err.Code, err.Message, err.Type.Value });
+
+                return StatusCode(500, new {err.Code, err.Message, err.Type.Value});
+            }
+
+            return Ok(result.Value);
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout(LogoutRequest request, CancellationToken cancellationToken = default)
+        {
+            var command = request.ToCommand();
+
+            await _logoutHandler.HandleAsync(command, cancellationToken);
+
+            return NoContent();
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh(RefreshRequest request, CancellationToken cancellationToken)
+        {
+            var command = request.ToCommand();
+
+            var result = await _refreshTokenHandler.HandleAsync(command, cancellationToken);
+
+            if (result.IsFailure)
+                return Unauthorized(new { result.Error!.Code, result.Error.Message, result.Error.Type.Value });
+
+            return Ok(result.Value);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var command = request.ToCommand();
+            var result = await _registerHandler.HandleAsync(command, default);
+
+            if (result.IsSuccess)
+                return Ok();
+
+            var err = new { result.Error?.Code, result.Error?.Message, Type = result.Error?.Type.Value };
+
+            if (result.Error?.Type == ErrorType.Conflict)
+                return Conflict(err);
+
+            if (result.Error?.Type == ErrorType.Validtion)
+                return BadRequest(err);
+
+            return StatusCode(500, err);
+        }
+
+        [HttpPost("verify-otp")]
+        public async Task<IActionResult> VerifyOtp(VerifyOtpRequest request)
+        {
+            var command = request.ToCommand();
+            var result = await _verifyOtpHandler.HandleAsync(command, default);
+
+            if (result.IsSuccess)
+                return Created();
+
+            if (result.Error?.Type == ErrorType.Validtion ||
+                result.Error?.Type == ErrorType.Business)
+                return BadRequest(result.Error);
+            return StatusCode(500, result.Error);
+        }
+    }
+}
--- src/FastFoodOrderingSystem.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/FastFoodOrderingSystem.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 using FastFoodOrderingSystem.Api.Middlewares;
+using FastFoodOrderingSystem.Infrastructure.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FastFoodOrderingSystem.Api.Extensions;
 
@@ -6,7 +9,54 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddAuthentication();
+        var provider = services.BuildServiceProvider();
+        
+        services.Configure<RouteOptions>(config =>
+        {
+            config.LowercaseUrls = true;
+        });
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer((options) =>
+            {
+                var jwtOptions = provider.GetRequiredService<JwtOption>();
+
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtOptions.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = jwtOptions.Audience,
+
+                    ValidateLifetime = true,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtOptions.Key))
+                };
+
+                options.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        var logger = provider.GetRequiredService<ILogger>();
+                        logger.LogWarning(context.Exception, "JWT validation failed.");
+                        return Task.CompletedTask;
+                    },
+
+                    OnForbidden = async context =>
+                    {
+                        await context.Response.WriteAsJsonAsync(new
+                        {
+                            Message = "Permission denied."
+                        });
+                    }
+                };
+            });
 
         services.AddAuthorization();
         
