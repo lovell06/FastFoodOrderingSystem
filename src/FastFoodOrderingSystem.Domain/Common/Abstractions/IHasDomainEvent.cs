a4d50246a57f0d0594951ceae793c0bd68935aa3
lovell06 <xuansontrannguyen06@gmail.com>
2026-07-21 15:41:51 +0700

    rename User.RegisterDomainEvent -> User.PublishDomainEvent

--- src/FastFoodOrderingSystem.Domain/Common/Abstractions/IHasDomainEvent.cs
+++ src/FastFoodOrderingSystem.Domain/Common/Abstractions/IHasDomainEvent.cs
@@ -3,6 +3,6 @@ namespace FastFoodOrderingSystem.Domain.Common.Abstractions;
 public interface IHasDomainEvent
 {
     IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
-    void RegisterDomainEvent(IDomainEvent domainEvent);
+    void PublishDomainEvent(IDomainEvent domainEvent);
     void ClearDomainEvent();
 }
